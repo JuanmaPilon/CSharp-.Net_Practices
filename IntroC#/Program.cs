c213eaecbc7127908b9200022b29842cecc7c84b
JuanmaPilon <juanmartin.pilon@gmail.com>
2025-03-11 16:17:19 -0300

    Adding first things that ive tested

---
+++ IntroC#/Program.cs
@@ -0,0 +1,35 @@
+﻿
+var sale = new Sale(15);
+var message = sale.GetInfo();
+var sumado = sale.Sumar(30);
+
+Console.WriteLine(sumado);
+
+
+
+class Sale
+{
+    public decimal Total { get; set; }
+
+    public Sale(decimal total)
+    {
+        Total = total;
+    }
+
+    public string GetInfo()
+    {
+        return "El total es " + Total;
+    }
+
+    public decimal GetTotal()
+    {
+        return Total;
+    }
+
+    public decimal Sumar(decimal a)
+    {
+        return Total + a;
+    }
+}
+
+
