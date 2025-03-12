c66e96a236a1aa27bc74020a80569e2cd86dc95c
JuanmaPilon <juanmartin.pilon@gmail.com>
2025-03-12 16:33:44 -0300

    looking over the basics of C#

--- IntroC#/Program.cs
+++ IntroC#/Program.cs
@@ -1,11 +1,72 @@
 ﻿
-var sale = new Sale(15);
+var sale = new Sale();
+var game = new Game();
+
+Some(sale);
+Some(game);
+
+void Some(ISave save)
+{
+    save.Save();
+}
+
+interface ISale
+{
+    decimal Total { get; set; 
+}
+
+
+interface ISave
+    {
+        public void Save();
+    }
+
+    public class  Sale : ISale, ISave
+    { 
+      public decimal Total { get; set; }
+
+    public void Save()
+        {
+            Console.WriteLine("Guardando");
+        }
+    }
+
+    public class Game : ISave
+    {
+        public void Save()
+        {
+            Console.WriteLine("Guardando");
+        }
+    }
+}
+
+
+
+var sale = new SaleWithTax(15, 1.15m);
 var message = sale.GetInfo();
 var sumado = sale.Sumar(30);
 
 Console.WriteLine(sumado);
 
 
+class SaleWithTax : Sale {
+
+    public decimal Tax { get; set; }
+
+    public SaleWithTax(decimal total, decimal tax) : base(total){
+        Tax = tax;
+    
+    }
+    public override string GetInfo()
+    {
+        return "El total es " + Total + "El impuesto es: " + Tax;
+    }
+
+    public string GetInfo(string message)
+    {
+        return message;
+    }
+}
 
 class Sale
 {
@@ -16,7 +77,7 @@ class Sale
         Total = total;
     }
 
-    public string GetInfo()
+    public virtual string GetInfo()
     {
         return "El total es " + Total;
     }
--- IntroC#/obj/Release/net8.0/IntroC#.AssemblyInfo.cs
+++ IntroC#/obj/Release/net8.0/IntroC#.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("IntroC#")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c213eaecbc7127908b9200022b29842cecc7c84b")]
 [assembly: System.Reflection.AssemblyProductAttribute("IntroC#")]
