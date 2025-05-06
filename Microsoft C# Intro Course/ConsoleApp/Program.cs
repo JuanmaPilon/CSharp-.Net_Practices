4cf33c4fb3bed3f55582a1962c6715133e111274
JuanmaPilon <juanmartin.pilon@gmail.com>
2025-05-06 13:00:21 -0300

    more excersises

--- Microsoft C# Intro Course/ConsoleApp/Program.cs
+++ Microsoft C# Intro Course/ConsoleApp/Program.cs
@@ -3,18 +3,40 @@ using Pastel;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
-public class Calculator {
+// public class Calculator {
 
-    public static int Add(int a, int b) {
-        return a + b;
-    }
+//     public static int Add(int a, int b) {
+//         return a + b;
+//     }
 
-    static void Main(string[] args) {
-        Console.WriteLine("Hello, World!".Pastel(Color.Blue));
+//     static void Main(string[] args) {
+//         Console.WriteLine("Hello, World!".Pastel(Color.Blue));
 
-        int result = Add(5, 10);
-        Console.WriteLine($"The result is: {result}".Pastel(Color.Green));
-        Console.WriteLine("Press any key to exit...".Pastel(Color.Red));
-        Console.ReadKey();
-    }
-}
+//         int result = Add(5, 10);
+//         Console.WriteLine($"The result is: {result}".Pastel(Color.Green));
+//         Console.WriteLine("Press any key to exit...".Pastel(Color.Red));
+//         Console.ReadKey();
+//     }
+// }
+
+// public class NumberDisplay {
+//     public static void DisplayNumbers() {
+//         for (int i = 1;i <= 10; i++) {
+//             Console.WriteLine(i.ToString().Pastel(Color.Yellow));
+//         }
+//     }
+
+//     static void Main(string[] args) {
+//         DisplayNumbers();
+//         Console.WriteLine("Press any key to exit...".Pastel(Color.Red));
+//         Console.ReadKey();
+//     }
+// }
+
+// public class UserInput {
+//     static void Greetings() {
+//         Console.WriteLine("Enter your name: ".Pastel(Color.Blue));
+//         string name = Console.ReadLine();
+//         Console.WriteLine($"Hello, {name}!".Pastel(Color.Green));
+//     }
+// }
