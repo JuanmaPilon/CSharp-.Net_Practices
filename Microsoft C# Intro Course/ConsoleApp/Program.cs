eac2a6fb4dea9cdfd4c838888a23800089fe0922
JuanmaPilon <juanmartin.pilon@gmail.com>
2025-05-06 11:56:42 -0300

    keeping up with the course

--- Microsoft C# Intro Course/ConsoleApp/Program.cs
+++ Microsoft C# Intro Course/ConsoleApp/Program.cs
@@ -1,5 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System.Drawing;
+﻿using System.Drawing;
 using Pastel;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
-Console.WriteLine("Hello, World!".Pastel(Color.Blue));
+public class Calculator {
+
+    public static int Add(int a, int b) {
+        return a + b;
+    }
+
+    static void Main(string[] args) {
+        Console.WriteLine("Hello, World!".Pastel(Color.Blue));
+
+        int result = Add(5, 10);
+        Console.WriteLine($"The result is: {result}".Pastel(Color.Green));
+        Console.WriteLine("Press any key to exit...".Pastel(Color.Red));
+        Console.ReadKey();
+    }
+}
